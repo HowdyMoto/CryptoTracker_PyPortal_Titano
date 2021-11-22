05e42a5bf84f11f321767450ce7f3c2e25857a62
Wright Bagwell <wwbagwell@hotmail.com>
2021-11-21 22:17:44 -0800

    Refactored so that getting price  is now a function
    Now, instead of hard-coding the three requests, there's a single function that is called three times.
    Background now turns red when error state is detected, prompting user to restart the device.
    Secrets.py now expected to have coin names and labels.

--- code.py
+++ code.py
@@ -2,11 +2,9 @@ import time
 import board
 import busio
 import displayio
-import terminalio
 from digitalio import DigitalInOut
 
 from adafruit_display_text import label
-from adafruit_display_shapes.triangle import Triangle
 from adafruit_display_shapes.rect import Rect
 from adafruit_bitmap_font import bitmap_font
 
@@ -20,62 +18,62 @@ WHITE = 0xFFFFFF
 BLACK = 0x000000
 GREEN = 0x00FF00
 RED =   0xFF0000
-btc_price = 0.0
-eth_price = 0.0
-xmr_price = 0.0
+DARKRED =   0xCC0000
+URL = "http://api.coincap.io/v2/assets/"
 BTC_URL = "http://api.coincap.io/v2/assets/bitcoin"
 ETH_URL = "http://api.coincap.io/v2/assets/ethereum"
 XMR_URL = "http://api.coincap.io/v2/assets/monero"
-num_requests = 0
-num_loops = 0
+NUM_LOOPS = 0
 
-########### Set up display ###############################################################################
+######## Get info from secrets.py ######################################################
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi credentials, API keys, and assets tracked are kept in secrets.py, please add them there!")
+    raise
+
+########### Set up display and load screen UI ################################################################
 display = board.DISPLAY
-
-########## UI setup ##########################################################################################
 font = bitmap_font.load_font("fonts/Nunito-Regular-75.bdf")
 loadscreen_group = displayio.Group()
 wait_label = label.Label(font, text="Connecting to WiFi...", scale=1, color=WHITE, x=80, y=150)
 loadscreen_group.append(wait_label)
 display.show(loadscreen_group)
 
+########## UI setup ##########################################################################################
 indent_label = 32
 indent_price = 140
 indent_change = 360
 indent_indicator = 400
 indent_top = 64
 vert_spacing = 96
-triangle_width = 32
+
+rect_background = Rect(0, 0, display.width, display.height, fill=BLACK) # If we detect an error, we turn the background red.
 
 pricedata_group = displayio.Group()
-btc_label = label.Label(font, text="BTC", color=WHITE, x=indent_label, y=indent_top )
-eth_label = label.Label(font, text="ETH", color=WHITE, x=indent_label, y=(indent_top + vert_spacing) )
-xmr_label = label.Label(font, text="XMR", color=WHITE, x=indent_label, y=(indent_top + vert_spacing*2) )
-btc_price_label = label.Label(font, text="wait...", scale=1, color=WHITE, x=indent_price, y=indent_top)
-eth_price_label = label.Label(font, text="wait...", scale=1, color=WHITE, x=indent_price, y=(indent_top + vert_spacing))
-xmr_price_label = label.Label(font, text="wait...", scale=1, color=WHITE, x=indent_price, y=(indent_top + vert_spacing*2) )
-btc_change_label = label.Label(font, text=str(""), scale=1, color=WHITE, x=indent_change, y=indent_top)
-eth_change_label = label.Label(font, text=str(""), scale=1, color=WHITE, x=indent_change, y=(indent_top + vert_spacing))
-xmr_change_label = label.Label(font, text=str(""), scale=1, color=WHITE, x=indent_change, y=(indent_top + vert_spacing*2) )
+label_coin1 = label.Label(font, text=secrets["coin1label"], color=WHITE, x=indent_label, y=indent_top )
+label_coin2 = label.Label(font, text=secrets["coin2label"], color=WHITE, x=indent_label, y=(indent_top + vert_spacing) )
+label_coin3 = label.Label(font, text=secrets["coin3label"], color=WHITE, x=indent_label, y=(indent_top + vert_spacing*2) )
+label_coin1_price = label.Label(font, text="wait...", scale=1, color=WHITE, x=indent_price, y=indent_top)
+label_coin2_price = label.Label(font, text="wait...", scale=1, color=WHITE, x=indent_price, y=(indent_top + vert_spacing))
+label_coin3_price = label.Label(font, text="wait...", scale=1, color=WHITE, x=indent_price, y=(indent_top + vert_spacing*2) )
+label_coin1_change = label.Label(font, text=str(""), scale=1, color=WHITE, x=indent_change, y=indent_top)
+label_coin2_change = label.Label(font, text=str(""), scale=1, color=WHITE, x=indent_change, y=(indent_top + vert_spacing))
+label_coin3_change = label.Label(font, text=str(""), scale=1, color=WHITE, x=indent_change, y=(indent_top + vert_spacing*2) )
 
-pricedata_group.append(btc_label)
-pricedata_group.append(eth_label)
-pricedata_group.append(xmr_label)
-pricedata_group.append(btc_price_label)
-pricedata_group.append(eth_price_label)
-pricedata_group.append(xmr_price_label)
-pricedata_group.append(btc_change_label)
-pricedata_group.append(eth_change_label)
-pricedata_group.append(xmr_change_label)
+pricedata_group.append(rect_background)
+pricedata_group.append(label_coin1)
+pricedata_group.append(label_coin2)
+pricedata_group.append(label_coin3)
+pricedata_group.append(label_coin1_price)
+pricedata_group.append(label_coin2_price)
+pricedata_group.append(label_coin3_price)
+pricedata_group.append(label_coin1_change)
+pricedata_group.append(label_coin2_change)
+pricedata_group.append(label_coin3_change)
 
 ######## Wi-Fi setup ########################################################################################
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi SSID and password are kept in secrets.py, please add them there!")
-    raise
 
-# If you are using a board with pre-defined ESP32 Pins:
 esp32_cs = DigitalInOut(board.ESP_CS)
 esp32_ready = DigitalInOut(board.ESP_BUSY)
 esp32_reset = DigitalInOut(board.ESP_RESET)
@@ -103,78 +101,39 @@ header = {'Authorization': 'Bearer ' + api_key}
 
 display.show(pricedata_group)   # Only show once the bootup sequence is done and data is requested.
 
-######## MAIN LOOP ###########################################################################################
+######### Get the price of a coin ########################################################################
+def getprice(asset, pricelabel, changelabel):
+    try:
+        response = requests.get(URL + asset, headers=header)
+        response_json = response.json()
+
+        price_unformatted = float(response_json["data"]["priceUsd"])
+        price_delta_unformatted = float(response_json["data"]["changePercent24Hr"])    # get the percentage change, which can be many decimal places
+
+        price = "%.2f" % price_unformatted                            # Create a string from a float and round the price to 2 decimal places
+        price_delta = "%.1f" % price_delta_unformatted                # Round the price to 1 decimal place 
+
+        if price_delta_unformatted >= 0:
+            pricelabel.color = GREEN
+            changelabel.color = GREEN
+        else:
+            pricelabel.color = RED
+            changelabel.color = RED
+
+        pricelabel.text = price
+        changelabel.text = price_delta + "%"
+
+    except (ValueError, RuntimeError) as e:
+        print("Error: ", e)
+        rect_background.fill = (128,0,0)
+
+######## MAIN LOOP #########################################################################################
 while True:
 
+    getprice("bitcoin", label_coin1_price, label_coin1_change)
+    getprice("ethereum", label_coin2_price, label_coin2_change)
+    getprice("monero", label_coin3_price, label_coin3_change)
 
-    ######## BTC prices
-    response = requests.get(BTC_URL, headers=header)
-    num_requests += 1
-    try:
-        response_json = response.json()
-        btc_price = float(response_json["data"]["priceUsd"])
-        btc_price_label.text = "%.2f" % btc_price
-        btc_price_delta_unformatted = float(response_json["data"]["changePercent24Hr"])
-        btc_price_delta = round(btc_price_delta_unformatted, 1)
-        if btc_price_delta >= 0:
-            btc_price_label.color = GREEN
-            btc_change_label.color = GREEN
-            btc_change_label.text = str(btc_price_delta) + "%"
-        else:
-            btc_price_label.color = RED
-            btc_change_label.color = RED
-            btc_change_label.text = str(btc_price_delta) + "%"
-    except (ValueError, RuntimeError) as e:
-        print(str(response))
-        print("Server not responding, retrying in 60 seconds. -", e)
-        time.sleep(60)
-
-
-    ######## ETH prices
-    response = requests.get(ETH_URL, headers=header)
-    num_requests += 1
-    try:
-        response_json = response.json()
-        eth_price = float(response_json["data"]["priceUsd"])
-        eth_price_label.text = "%.2f" % eth_price
-        eth_price_delta_unformatted = float(response_json["data"]["changePercent24Hr"])
-        eth_price_delta = round(eth_price_delta_unformatted, 1)
-        if eth_price_delta >= 0.0:
-            eth_price_label.color = GREEN
-            eth_change_label.color = GREEN
-            eth_change_label.text = str(eth_price_delta) + "%"
-        else:
-            eth_price_label.color = RED
-            eth_change_label.color = RED
-            eth_change_label.text = str(eth_price_delta) + "%"
-    except (ValueError, RuntimeError) as e:
-        print(str(response))
-        print("Server not responding, retrying in 60 seconds. -", e)
-        time.sleep(60)
-
-
-    ######## XMR prices
-    response = requests.get(XMR_URL, headers=header)
-    num_requests += 1
-    try:
-        response_json = response.json()
-        xmr_price = float(response_json["data"]["priceUsd"])
-        xmr_price_label.text = "%.2f" % xmr_price
-        xmr_price_delta_unformatted = float(response_json["data"]["changePercent24Hr"])
-        xmr_price_delta = round(xmr_price_delta_unformatted, 1)
-        if xmr_price_delta >= 0.0:
-            xmr_price_label.color = GREEN
-            xmr_change_label.color = GREEN
-            xmr_change_label.text = str(xmr_price_delta) + "%"
-        else:
-            xmr_price_label.color = RED
-            xmr_change_label.color = RED
-            xmr_change_label.text = str(xmr_price_delta) + "%"
-    except (ValueError, RuntimeError) as e:
-        print(str(response))
-        print("Server not responding, retrying in 60 seconds. -", e)
-        time.sleep(60)
-
-    num_loops += 1
-    print("Loops=" + str(num_loops) + " Requests=" + str(num_requests) )
+    NUM_LOOPS += 1
+    print("Loops=" + str(NUM_LOOPS) )
     time.sleep(180)
